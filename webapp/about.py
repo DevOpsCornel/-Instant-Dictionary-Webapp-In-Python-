1509a275bd2ba86e67c63fffda13adb4a49961ba
DevOpsCornel <cloudcornelius2023@gmail.com>
2023-08-31 04:50:45 +0100

    Another commit to this awesome project

--- webapp/about.py
+++ webapp/about.py
@@ -1,10 +1,18 @@
 import justpy as jp
+from webapp import layout
+
 
 class About:
     path = "/about"
+
     def serve(self):
         wp = jp.QuasarPage(tailwind=True)
-        div = jp.Div(a=wp, classes="bg-gray-200 h-screen")
+
+        lay = layout.DefaultLayout(a=wp)
+
+        container = jp.QPageContainer(a=lay)
+
+        div = jp.Div(a=container, classes="bg-gray-200 h-screen")
         jp.Div(a=div, text="This is the about page", classes="text-4xl m-2")
         jp.Div(a=div, text="""It is a long established fact
          that a reader will be distracted by the readable
@@ -15,9 +23,3 @@ class About:
           making it look like readable English. """,
                classes="text-lg")
         return wp
-
-
-
-
-
-
